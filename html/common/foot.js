c24bbe4736c1ee58562f5133fa6aca96e6e9ed6d
huzhongyuan <1141422520@qq.com>
2018-08-20 19:19:57 +0800

    基本完成

---
+++ html/common/foot.js
@@ -0,0 +1,121 @@
+let foot ='<footer class="aui-bar aui-bar-tab footer" id="footer">' +
+
+'</footer>' +
+'</body>' +
+'</html>';
+document.write(foot);
+
+
+let to_back = document.getElementById('to_back');
+to_back.style.visibility = 'hidden';
+
+//返回上一层
+back();
+//点击底部导航跳转
+let footNav = () => {
+    let tohome = document.getElementsByClassName('tohome')[0];
+    let toteam = document.getElementsByClassName('toteam')[0];
+    let tocase = document.getElementsByClassName('tocase')[0];
+    let tocompany = document.getElementsByClassName('tocompany')[0];
+    tohome.onclick = (event) => {
+        sessionStorage.setItem('local', 0);
+        window.location.href = './home.html';
+        event.stopPropagation();
+    }
+    toteam.onclick = (event) => {
+        sessionStorage.setItem('local', 1);
+        window.location.href = './team.html';
+        event.stopPropagation();
+    }
+    tocase.onclick = (event) => {
+        sessionStorage.setItem('local', 2);
+        window.location.href = './case.html';
+        event.stopPropagation();
+    }
+    tocompany.onclick = (event) => {
+        sessionStorage.setItem('local', 3);
+        window.location.href = './company.html';
+        event.stopPropagation();
+    }
+}
+// sessionStorage.setItem('resFooter', '');
+// console.log(sessionStorage.getItem('resFooter'));
+
+let onfocus = () => {
+    let local = sessionStorage.getItem('local');
+    local = parseInt(local);
+    //console.log(local);
+    let resFooter = sessionStorage.getItem('resFooter');
+    resFooter = JSON.parse(resFooter);
+    resFooter[local].addImgUrl = resFooter[local].imgUrl;
+    return resFooter;
+}
+
+//写入头部和底部数据
+let writeCommon = (resFooter, resHeader) => {
+    let footerNav = document.createElement('div');
+    footerNav.innerHTML = '<div class="aui-bar-tab-item footerNav tohome" tapmode>' +
+        '<img src="' + resFooter[0].addImgUrl + '" class="homeImg" style="width:1rem;height:1rem;">' +
+        '<div  class="aui-bar-tab-label">' + resFooter[0].columnName + '</div>' +
+    '</div>' +
+    '<div class="aui-bar-tab-item footerNav toteam" tapmode>' +
+        '<img src="' + resFooter[1].addImgUrl + '" style="width:1rem;height:1rem;" class="teamImg">' +
+        '<div class="aui-bar-tab-label">' +resFooter[1].columnName+ '</div>'+
+    '</div>' +
+    '<div class="aui-bar-tab-item footerNav tocase" tapmode>' +
+        '<img src="' + resFooter[2].addImgUrl + '"  style="width:1rem;height:1rem;" class="caseImg">' +
+        '<div class="aui-bar-tab-label">' + resFooter[2].columnName + '</div>' +
+    '</div>' +
+    '<div class="aui-bar-tab-item footerNav tocompany" tapmode>' +
+        '<img src="' + resFooter[3].addImgUrl + '"  style="width:1rem;height:1rem;" class="companyImg">' +
+        '<div class="aui-bar-tab-label">' + resFooter[3].columnName + '</div>' +
+    '</div>';
+    let footer = document.getElementById('footer');
+    footer.style.backgroundColor = 'rgba(0,0,0,1)';
+    footer.appendChild(footerNav);
+    let header = document.getElementsByClassName('headerTitle')[0];
+    header.innerHTML = resHeader.name;
+}
+
+// sessionStorage.setItem('resFooter', '');
+//储存数据
+if(sessionStorage.getItem('resFooter') && sessionStorage.getItem('resFooter')) {
+    //console.log(1111);
+    let resFooter = sessionStorage.getItem('resFooter');
+    let resHeader = sessionStorage.getItem('resHeader');
+    resFooter = JSON.parse(resFooter);
+    resHeader = JSON.parse(resHeader);
+    resFooter = onfocus();
+    writeCommon(resFooter, resHeader);
+    footNav();
+}else {
+//首次进入界面加载数据
+let footerImg = new Promise((resolve, reject) => {
+    $.ajax({
+        url: baseUrl + '/siteInfo',
+        dataType: 'json',
+        method: 'get',
+        success: function (res) {
+        console.log(res);
+        sessionStorage.setItem('resFooter', JSON.stringify(res.h5Footer));
+        sessionStorage.setItem('resHeader', JSON.stringify(res.siteInfo));
+        sessionStorage.setItem('local', 0);
+        resFooter = onfocus();
+        writeCommon(resFooter, res.siteInfo);
+        onfocus();
+        footNav();
+        resolve(1);
+        }
+    })
+}).then (() => {
+  siteInfo =  JSON.parse(sessionStorage.getItem('resHeader'));
+// 修改头部
+  document.title = siteInfo.name;
+  document.getElementsByTagName('meta')[6]['content'] = siteInfo.keywords;
+  document.getElementsByTagName('meta')[7]['content'] = siteInfo.description;
+  document.getElementsByTagName('link')[0]['href'] = siteInfo.icon;
+})
+}
+
+
+
---
+++ js/module/utils.js
@@ -0,0 +1,149 @@
+//当屏幕尺寸大于750px跳转到PC端
+let w = document.body.clientWidth;
+if (w >= 750) {
+  window.location.href = 'http://www.yaniaozg.com';
+}
+//将时间戳转化为日期格式
+function timestampToTime(timestamp) {
+  var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  Y = date.getFullYear() + '-';
+  M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+  D = date.getDate() + ' ';
+  h = date.getHours() + ':';
+  m = date.getMinutes() + ':';
+  s = date.getSeconds();
+  return Y + M + D + h + m + s;
+}
+
+//去除双引号
+let replaceQ = (str) => {
+  str = str.substring(1, str.length - 1);
+  return str;
+}
+
+//获取url参数
+function getQueryString(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]); return null;
+}
+
+//匹配富文本之间的文本
+let march = (s) => {
+  var dd = s.replace(/<\/?.+?>/g, "");
+  var dds = dd.replace(/ /g, "");//dds为得到后的内容
+  return dds;
+}
+// 设置常数
+const baseUrl = 'http://118.24.159.161:8083/h5';
+
+// 获取基本信息
+let timeStamp = Date.parse(new Date());
+let token = timeStamp + 'hopynrztoken';
+token = md5(token).toUpperCase();
+
+// 设置接口token
+$(document).ajaxSend(function (event, xhr) {
+  xhr.setRequestHeader("token", token);
+  xhr.setRequestHeader("timeStamp", timeStamp);
+});
+
+//返回上一页
+let back = () => {
+  let to_back = document.getElementById('to_back');
+  to_back.onclick = () => {
+    window.history.go(-1);
+  }
+}
+
+//得到头部标签
+let getHeader = () => {
+  if (sessionStorage.getItem('resHeader')) {
+    let header = document.getElementsByClassName('headerTitle')[0];
+    let resHeader = sessionStorage.getItem('resHeader');
+    resHeader = JSON.parse(resHeader);
+    header.innerHTML = resHeader.name;
+  } else {
+    $.ajax({
+      url: baseUrl + '/siteInfo',
+      dataType: 'json',
+      method: 'get',
+      success: function (res) {
+        let header = document.getElementsByClassName('headerTitle')[0];
+        header.innerHTML = res.siteInfo.name;
+      }
+    })
+  }
+}
+
+
+    //上拉加载
+    let upload = (that,loadMore) => {
+      //获取滚动条当前的位置 
+      function getScrollTop() {
+        var scrollTop = 0;
+        if (document.documentElement && document.documentElement.scrollTop) {
+          scrollTop = document.documentElement.scrollTop;
+        }
+        else if (document.body) {
+          scrollTop = document.body.scrollTop;
+        }
+        return scrollTop;
+      }
+      //获取当前可视范围的高度 
+      function getClientHeight() {
+        var clientHeight = 0;
+        if (document.body.clientHeight && document.documentElement.clientHeight) {
+          clientHeight = Math.min(document.body.clientHeight, document.documentElement.clientHeight);
+        }
+        else {
+          clientHeight = Math.max(document.body.clientHeight, document.documentElement.clientHeight);
+        }
+        return clientHeight;
+      }
+      //获取文档完整的高度 
+      function getScrollHeight() {
+        return Math.max(document.body.scrollHeight, document.documentElement.scrollHeight);
+      }
+      window.onscroll = function () {
+        //console.log(2222);
+        if (getScrollTop() + getClientHeight() == getScrollHeight()) {
+          //ajax从这里开始
+          //console.log(1111111)
+          if (!isload) {
+            loadMore(that);
+          }
+        }
+      }
+    }
+
+    // //图片加载错误
+    // function onfind(img) {
+    //   console.log(1111);
+    //   img.src="./../images/add.png";
+    //   img.onerror=null;
+    // }
+
+//图片加载失败显示默认图片
+let errorimg = (res) => {
+  for (let i in res) {
+    if (!res[i].show_url) {
+      res[i].show_url = 'http://www.watx365.com/img/zwimg.png';
+    }
+  }
+  return res;
+}
+
+
+// //设置图片加载失败默认图片
+// function imgNotfind(e) {
+//   console.log(111111);
+//   var img = e.srcElement;
+//   img.src = "http://www.watx365.com/img/zwimg.png";
+// }
+
+// //设置默认图片
+// function imgDefault(e) {
+//   var img = e.srcElement;
+//   img.src = "/img/forme3.png";
+// }
