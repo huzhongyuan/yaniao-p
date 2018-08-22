1157e2d28abba41f4bfb199b6d9d04773b2e5887
huzhongyuan <1141422520@qq.com>
2018-08-22 10:28:34 +0800

    时间戳修改

--- js/module/utils.js
+++ js/module/utils.js
@@ -4,15 +4,21 @@ if (w >= 750) {
   window.location.href = 'http://www.yaniaozg.com';
 }
 //将时间戳转化为日期格式
-function timestampToTime(timestamp) {
-  var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
-  Y = date.getFullYear() + '-';
-  M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-  D = date.getDate() + ' ';
-  h = date.getHours() + ':';
-  m = date.getMinutes() + ':';
-  s = date.getSeconds();
-  return Y + M + D + h + m + s;
+function timestampToTime(inputTime) {
+  //console.log(typeof(inputTime));
+  var date = new Date(inputTime);
+  var y = date.getFullYear();  
+  var m = date.getMonth() + 1;  
+  m = m < 10 ? ('0' + m) : m;  
+  var d = date.getDate();  
+  d = d < 10 ? ('0' + d) : d;  
+  var h = date.getHours();
+  h = h < 10 ? ('0' + h) : h;
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  minute = minute < 10 ? ('0' + minute) : minute;  
+  second = second < 10 ? ('0' + second) : second; 
+  return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
 }
 
 //去除双引号
